2cc9d707df1578b87f0159e65c7f2dd9e9747274
Sogand <sogandmohamadipour7@gmail.com>
2025-09-29 22:14:55 +0330

    component composition

--- src/App.js
+++ src/App.js
@@ -51,21 +51,30 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <Navbar />
-      <Main />
+      <Navbar>
+        <Serach />
+        <NumResults movies={movies} />
+      </Navbar>
+      
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function Navbar() {
+function Navbar({ children }) {
   return (
     <>
       <nav className="nav-bar">
         <Logo />
-        <Serach />
-        <NumResults />
+        {children}
       </nav>
     </>
   )
@@ -97,28 +106,27 @@ function Serach() {
   )
 }
 
-function NumResults() {
+function NumResults({ movies }) {
   return (
     <>
       <p className="num-results">
-        Found <strong>X</strong> results
+        Found <strong>{movies.length}</strong> results
       </p>
     </>
   )
 }
 
-function Main() {
+function Main({ children }) {
   return (
     <>
       <main className="main">
-        <ListBox />
-        <WatchedBox />
+        {children}
       </main>
     </>
   )
 }
 
-function ListBox() {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <>
@@ -129,14 +137,13 @@ function ListBox() {
         >
           {isOpen1 ? "–" : "+"}
         </button>
-        {isOpen1 && <MovieList />}
+        {isOpen1 &&  children }
       </div>
     </>
   )
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
   return (
     <>
       <ul className="list">
@@ -231,7 +238,7 @@ function WatchedMovieList({ watched }) {
   )
 }
 
-function WatchedMovie({movie}) {
+function WatchedMovie({ movie }) {
   return (
     <>
       <li>
